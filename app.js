a613d91c0cc684b4020e4e0bf1ff860ef8b7a40b
OlamidotunIY <olamidotun225@gmail.com>
2023-02-20 11:41:47 +0100

    updated

--- app.js
+++ app.js
@@ -4,108 +4,84 @@ const projects = [
         category: "JavaScript",
         title: "Menu Project",
         img: "./images/menu-project.JPG",
-        desc: "I used JavaScript to categorize menu into different categories and display them according to their categories",
         liveLink: "",
-        sourceCode: "",
     },
     {
         id: 2,
         category: "JavaScript",
         title: "color fliper",
         img: "./images/color-fliper.jpg",
-        desc: "I used JavaScript to categorize menu into different categories and display them according to their categories",
         liveLink: "",
-        sourceCode: "",
     },
     {
         id: 3,
         category: "JavaScript",
         title: "Countdown Project",
         img: "./images/countdown.JPG",
-        desc: "I used JavaScript to categorize menu into different categories and display them according to their categories",
         liveLink: "",
-        sourceCode: "",
     },
     {
         id: 4,
         category: "JavaScript",
         title: "Counter Project",
         img: "./images/counter.JPG",
-        desc: "I used JavaScript to categorize menu into different categories and display them according to their categories",
         liveLink: "",
-        sourceCode: "",
     },
     {
         id: 5,
         category: "JavaScript",
         title: "FAQ Section",
         img: "./images/faq-section.JPG",
-        desc: "I used JavaScript to categorize menu into different categories and display them according to their categories",
         liveLink: "",
-        sourceCode: "",
     },
     {
         id: 6,
         category: "JavaScript",
         title: "Grocery Bud",
         img: "./images/grocery-bud.JPG",
-        desc: "I used JavaScript to categorize menu into different categories and display them according to their categories",
         liveLink: "",
-        sourceCode: "",
     },
     {
         id: 7,
         category: "JavaScript",
         title: "Lorem Ipsum Generator",
         img: "./images/lorem-ipsum.JPG",
-        desc: "I used JavaScript to categorize menu into different categories and display them according to their categories",
         liveLink: "",
-        sourceCode: "",
     },
     {
         id: 8,
         category: "JavaScript",
         title: "Model Box",
         img: "./images/model.JPG",
-        desc: "I used JavaScript to categorize menu into different categories and display them according to their categories",
         liveLink: "",
-        sourceCode: "",
     },
     {
         id: 9,
         category: "JavaScript",
         title: "Reviews Project",
         img: "./images/reviews.JPG",
-        desc: "I used JavaScript to categorize menu into different categories and display them according to their categories",
         liveLink: "",
-        sourceCode: "",
     },
     {
         id: 10,
         category: "JavaScript",
         title: "Scroll Project",
         img: "./images/scroll-project.JPG",
-        desc: "I used JavaScript to categorize menu into different categories and display them according to their categories",
         liveLink: "",
-        sourceCode: "",
     },
     {
         id: 11,
         category: "JavaScript",
         title: "Slider",
         img: "./images/slider.JPG",
-        desc: "I used JavaScript to categorize menu into different categories and display them according to their categories",
         liveLink: "",
-        sourceCode: "",
     },
     {
         id: 12,
         category: "Bootstrap",
         title: "Patrix Clone Websites",
         img: "./images/patrix-clone.JPG",
-        desc: "I used JavaScript to categorize menu into different categories and display them according to their categories",
         liveLink: "",
-        sourceCode: "",
     },
 ]
 let menuBtn = document.querySelector(".menuBtn")
@@ -137,6 +113,22 @@ window.addEventListener('scroll', function(){
     }else{
         navBar.classList.remove('fixed')
     }
+
+    // back to top
+    const backToTopButton = document.getElementById("back-to-top-btn");
+
+    if (window.pageYOffset > 100) {
+        // Show the button
+        backToTopButton.style.display = "flex";
+      } else {
+        // Hide the button
+        backToTopButton.style.display = "none";
+    }
+    
+    backToTopButton.addEventListener("click", () => {
+        // Scroll to the top of the page
+        window.scrollTo({ top: 0, behavior: "smooth" });
+    });
 })
 
 // Portfolio Fliter projects
@@ -154,14 +146,36 @@ function displayProjects(projects){
                     <img src=${project.img} alt=${project.title}>
                     <div class="link-con">
                         <a href=${project.liveLink} class="live-link"><h1>${project.title}</h1></a>
-                        <p class="desc">${project.desc}<p>
-                        <a href=${project.sourceCode} class="github-link"><h2>Click here for source code</h2></a>
+                        <i class="fa-solid fa-arrow-right"></i>
                     </div>
                 </div>`;
     })
     displayProjects = displayProjects.join('')
     container.innerHTML = displayProjects;
-  
+
+        // Hide project when more than 6
+    const projectCon = document.querySelectorAll('.project-con')
+
+    for (let i = 0; i < projectCon.length; i++) {
+        if (i >= 6) {
+            projectCon[i].classList.add('hide-project');
+
+            const moreBtn = document.querySelector('.more-btn')
+            const hideBtn = document.querySelector('.hide-btn')
+
+            moreBtn.addEventListener('click', function(){
+                projectCon[i].classList.remove('hide-project');
+                hideBtn.classList.remove('hide-project');
+                moreBtn.classList.add('hide-project');
+            })
+
+            hideBtn.addEventListener('click', function(){
+                projectCon[i].classList.add('hide-project');
+                hideBtn.classList.add('hide-project');
+                moreBtn.classList.remove('hide-project');
+            })
+        }
+      }
 }
 
 function displayMenuBtn(){
@@ -182,7 +196,6 @@ function displayMenuBtn(){
     filterBtns.forEach(function(btn){
         btn.addEventListener('click', function(e){
         const category = e.currentTarget.dataset.id;
-        console.log(category);
         const menuCategory = projects.filter(function(menuItem){
             if(menuItem.category === category){
               return menuItem
@@ -199,3 +212,55 @@ function displayMenuBtn(){
 
 
 }
+
+{/* Contact Form */}
+
+const contactForm = document.getElementById('contact-form'),
+      firstName = document.getElementById('firstName'),
+      lastName = document.getElementById('lastName'),
+      contactEmail = document.getElementById('email'),
+      projectName = document.getElementById('project-name'),
+      alart = document.getElementById('alart'),
+      projectDesc = document.getElementById('project-desc');
+
+const sendEmail = (e) => {
+    e.preventDefault()
+
+    // Check if the field has a value
+    if(firstName.value === '' || lastName.value === '' || contactEmail.value === '' || projectName.value === '' || projectDesc.value === ''){
+        // Add or remove color
+        alart.classList.remove('color-green')
+        alart.classList.add('color-red')
+        // show message
+        alart.textContent = 'Write all the input field'
+    }
+    else{
+        // serviceID - templateID - #form - publicKey
+        emailjs.sendForm('service_egl1w0r', 'template_xmh6y7g', '#contact-form', '05m8FYB7ZhMJXpB1h')
+            .then(() =>{
+                // Show message and color
+                alart.classList.remove('color-red')
+                alart.classList.add('color-green')
+
+                 // show message
+                alart.textContent = 'Message Sent'
+
+                // Remove message after 5 second
+                setTimeout(() =>{
+                    alart.textContent = ''
+                }, 5000)
+               
+            }, (error) =>{
+                alert(`OOPS SOMETHING FAILED....`, error)
+            })
+
+            // Clear Input field
+            firstName.value = ''
+            lastName.value = ''
+            contactEmail.value = ''
+            projectName.value = ''
+            projectDesc.value = ''
+    }
+
+}
+contactForm.addEventListener('submit', sendEmail);
